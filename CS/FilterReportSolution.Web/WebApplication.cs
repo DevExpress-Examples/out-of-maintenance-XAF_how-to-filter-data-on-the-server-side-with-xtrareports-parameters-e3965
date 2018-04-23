eb8d2a33c0995428d4d1a2d1ac3a885718caf501
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FilterReportSolution.Web/WebApplication.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Web;
+
+namespace FilterReportSolution.Web {
+    public partial class FilterReportSolutionAspNetApplication : WebApplication {
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
+        private FilterReportSolution.Module.FilterReportSolutionModule module3;
+        private FilterReportSolution.Module.Web.FilterReportSolutionAspNetModule module4;
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Security.SecurityStrategySimple securitySimple;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule module6;
+        private DevExpress.ExpressApp.Security.AuthenticationActiveDirectory authenticationActiveDirectory1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private DevExpress.ExpressApp.Reports.Web.ReportsAspNetModule reportsAspNetModule1;
+        private DevExpress.ExpressApp.Reports.ReportsModule reportsModule1;
+        private DevExpress.ExpressApp.Validation.ValidationModule module5;
+
+        public FilterReportSolutionAspNetApplication() {
+            InitializeComponent();
+        }
+
+        private void FilterReportSolutionAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+			e.Updater.Update();
+			e.Handled = true;
+#else
+            if (System.Diagnostics.Debugger.IsAttached) {
+                e.Updater.Update();
+                e.Handled = true;
+            } else {
+                string message = "The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application.\r\n" +
+                    "This error occurred  because the automatic database update was disabled when the application was started without debugging.\r\n" +
+                    "To avoid this error, you should either start the application under Visual Studio in debug mode, or modify the " +
+                    "source code of the 'DatabaseVersionMismatch' event handler to enable automatic database update, " +
+                    "or manually create a database using the 'DBUpdater' tool.\r\n" +
+                    "Anyway, refer to the following help topics for more detailed information:\r\n" +
+                    "'Update Application and Database Versions' at http://www.devexpress.com/Help/?document=ExpressApp/CustomDocument2795.htm\r\n" +
+                    "'Database Security References' at http://www.devexpress.com/Help/?document=ExpressApp/CustomDocument3237.htm\r\n" +
+                    "If this doesn't help, please contact our Support Team at http://www.devexpress.com/Support/Center/";
+
+                if (e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
+            this.module3 = new FilterReportSolution.Module.FilterReportSolutionModule();
+            this.module4 = new FilterReportSolution.Module.Web.FilterReportSolutionAspNetModule();
+            this.module5 = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.module6 = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            this.securitySimple = new DevExpress.ExpressApp.Security.SecurityStrategySimple();
+            this.authenticationActiveDirectory1 = new DevExpress.ExpressApp.Security.AuthenticationActiveDirectory();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.reportsAspNetModule1 = new DevExpress.ExpressApp.Reports.Web.ReportsAspNetModule();
+            this.reportsModule1 = new DevExpress.ExpressApp.Reports.ReportsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // module5
+            // 
+            this.module5.AllowValidationDetailsAccess = true;
+            // 
+            // securitySimple
+            // 
+            this.securitySimple.Authentication = this.authenticationActiveDirectory1;
+            this.securitySimple.UserType = typeof(DevExpress.ExpressApp.Security.SecuritySimpleUser);
+            // 
+            // authenticationActiveDirectory1
+            // 
+            this.authenticationActiveDirectory1.CreateUserAutomatically = true;
+            this.authenticationActiveDirectory1.LogonParametersType = null;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=(local);Initial Catalog=FilterReportSolution;Integrated Security=SSPI" +
+                ";Pooling=false";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // reportsModule1
+            // 
+            this.reportsModule1.EnableInplaceReports = true;
+            this.reportsModule1.ReportDataType = typeof(DevExpress.ExpressApp.Reports.ReportData);
+            // 
+            // FilterReportSolutionAspNetApplication
+            // 
+            this.ApplicationName = "FilterReportSolution";
+            this.Connection = this.sqlConnection1;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module6);
+            this.Modules.Add(this.securityModule1);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.module5);
+            this.Modules.Add(this.reportsModule1);
+            this.Modules.Add(this.reportsAspNetModule1);
+            this.Security = this.securitySimple;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.FilterReportSolutionAspNetApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
---
+++ VB/FilterReportSolution.Module.Web/WebModule.vb
@@ -0,0 +1,15 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+
+Namespace FilterReportSolution.Module.Web
+	<ToolboxItemFilter("Xaf.Platform.Web")> _
+	Public NotInheritable Partial Class FilterReportSolutionAspNetModule
+		Inherits ModuleBase
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
