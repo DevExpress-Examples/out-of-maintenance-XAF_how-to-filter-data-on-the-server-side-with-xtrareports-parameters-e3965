b09e34905b83287c5b6e3e4cd38bb7f06b26c7e5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FilterReportSolution.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace FilterReportSolution.Module {
+    public sealed partial class FilterReportSolutionModule : ModuleBase {
+        public FilterReportSolutionModule() {
+            InitializeComponent();
+        }
+    }
+}
